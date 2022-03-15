ddf598654db5a3d6aa7dbaea9ae3d0b21ba5662a
cdkrieg <cdkrieg@outlook.com>
2022-03-14 21:40:18 -0500

    changed validation.js functions to methods inside Validation class

--- validation.js
+++ validation.js
@@ -1,36 +1,36 @@
 "use strict"
 
-function numberOfPlayersValidation(input){
-    if(parseInt(input) > 0 && parseInt(input) < 10){
-        return true;
-    } else {return false}
+class Validate{
+    numberOfPlayersValidation(input){
+        if(parseInt(input) > 0 && parseInt(input) < 10){
+            return true;
+        } else {return false}
+    }
+
+    numberOfGamesValidation(input){
+        if(parseInt(input) === 3 || parseInt(input) === 5){
+            return true;
+        } else {return false}
+    }
+
+    gestureSelectedValidation(input){
+        if(parseInt(input >= 0) && parseInt(input) < 5){
+            return true;
+        } else {return false}
+    }
+
+    promptFor(question, valid) {
+        let isValid;
+        do {
+            var response = prompt(question);
+            if (response !== null) {
+            response.trim();
+            }
+            isValid = valid(response);
+        } while (response === "" || isValid === false || response === null);
+
+        return response;
+    }
 }
 
-function numberOfGamesValidation(input){
-    if(parseInt(input) === 3 || parseInt(input) === 5){
-        return true;
-    } else {return false}
-}
-
-function gestureSelectedValidation(input){
-    if(parseInt(input >= 0) && parseInt(input) < 5){
-        return true;
-    } else {return false}
-}
-
-function promptFor(question, valid) {
-    let isValid;
-    do {
-        var response = prompt(question);
-        if (response !== null) {
-        response.trim();
-        }
-        isValid = valid(response);
-    } while (response === "" || isValid === false || response === null);
-
-    return response;
-}
-
-module.exports.numberOfPlayersValidation = numberOfPlayersValidation;
-module.exports.numberOfGamesValidation = numberOfGamesValidation;
-module.exports.gestureSelectedValidation = gestureSelectedValidation;
+module.exports.Validate = Validate;
