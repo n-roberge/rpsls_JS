e790a5050dd74adc8533c255201bf8a9cd5e120f
cdkrieg <cdkrieg@outlook.com>
2022-03-14 20:47:38 -0500

    added validation.js and functions to validate user inputs

---
+++ validation.js
@@ -0,0 +1,23 @@
+"use strict"
+
+function numberOfPlayersValidation(input){
+    if(parseInt(input > 0) && parseInt(input) < 10){
+        return true;
+    } else {return false}
+}
+
+function numberOfGamesValidation(input){
+    if(parseInt(input === 3) || parseInt(input) === 5){
+        return true;
+    } else {return false}
+}
+
+function gestureSelectedValidation(input){
+    if(parseInt(input >= 0) && parseInt(input) < 5){
+        return true;
+    } else {return false}
+}
+
+module.exports.numberOfPlayersValidation = numberOfPlayersValidation;
+module.exports.numberOfGamesValidation = numberOfGamesValidation;
+module.exports.gestureSelectedValidation = gestureSelectedValidation;
